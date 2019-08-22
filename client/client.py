11a3d4c31b367f6caa84d6a20e80cd2a4a15ee4f
vashist99 <vashistgh99@gmail.com>
2019-08-22 07:39:14 +0530

    added iterations

--- client/client.py
+++ client/client.py
@@ -4,70 +4,80 @@ from Crypto.PublicKey.RSA import RSAImplementation
 import time
 #from os import stat,remove
 
+sum=0
+
 #generating key obj
 tup = RSA.generate(bits=2048,e=65537)
 
-#timer started:
-start_time = time.time()
+
+
 
 #connecting to server
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 HOST = '127.0.0.1'
-PORT = 56376
+PORT = 56377
 s.connect((HOST,PORT))
 print('connected to server')
 
-
-#recieving key object from server:
-tups = s.recv(2048)
-ts = RSA.importKey(tups)
-print('key object recieved key object from server')
-
-
-#encrypting the file:
-f = open('answers.txt','rb')
-data = f.read()
-f.close()
-#encrypting new.png
-enc_data = RSA.pubkey.pubkey.encrypt(ts,data,3)
-#storeD in enc_file.png
-f2 = open('enc_file.txt','wb')
-f2.write(enc_data[0])
-f2.close()
-print('file encrypted')
-
-#sending file to server:
-f2 = open('enc_file.txt','rb')
-s.sendfile(f2)
-print('encrypted file sent')
-f2.close()
+for i in range(20):
+    temp=0
+    #timer started:
+    start_time = time.time()
+    #recieving key object from server:
+    tups = s.recv(2048)
+    ts = RSA.importKey(tups)
+    print('key object recieved key object from server')
 
 
+    #encrypting the file:
+    f = open('answers.txt','rb')
+    data = f.read()
+    f.close()
+    #encrypting new.png
+    enc_data = RSA.pubkey.pubkey.encrypt(ts,data,3)
+    #storeD in enc_file.png
+    f2 = open('enc_file.txt','wb')
+    f2.write(enc_data[0])
+    f2.close()
+    print('file encrypted')
+
+    #sending file to server:
+    f2 = open('enc_file.txt','rb')
+    s.sendfile(f2)
+    print('encrypted file sent')
+    f2.close()
 
 
-#sending key obj to server:
-s.send(tup.exportKey('PEM'))
-print('key object sent to server')
 
-#recieving acknowledgement from server:
-ack_enc = s.recv(10000000) 
-ackf = open('ack_enc.txt','wb')
-ackf.write(ack_enc)
-ackf.close()
-print('encrypted ack file recieved')
 
-#decrypting ack file:
-f2 = open('ack_enc.txt','rb')
-data = f2.read()
-data2 = RSA.pubkey.pubkey.decrypt(tup,data)
-f2.close()
-f = open('final.txt','wb')
-f.write(data2)
-f.close()
-print('ack decrypted')
+    #sending key obj to server:
+    s.send(tup.exportKey('PEM'))
+    print('key object sent to server')
+
+    #recieving acknowledgement from server:
+    ack_enc = s.recv(10000000) 
+    ackf = open('ack_enc.txt','wb')
+    ackf.write(ack_enc)
+    ackf.close()
+    print('encrypted ack file recieved')
+
+    #decrypting ack file:
+    f2 = open('ack_enc.txt','rb')
+    data = f2.read()
+    data2 = RSA.pubkey.pubkey.decrypt(tup,data)
+    f2.close()
+    f = open('final.txt','wb')
+    f.write(data2)
+    f.close()
+    print('ack decrypted')
+    #end timer
+    end_time = time.time()
+    temp = (end_time-start_time)
+    sum += temp
+
 
 s.close()
-#end timer
-end_time = time.time()
 
-print('total time taken: ',end_time-start_time)
+
+
+print('average time taken for 10 iteration for this file size ',sum/10)
--- server/server.py
+++ server/server.py
@@ -1,11 +1,13 @@
 import socket
 from Crypto.PublicKey import RSA
+import time
+
 
 #generating key obj
 tup = RSA.generate(bits=2048,e=65537)
 
 HOST = '127.0.0.1'
-PORT = 56376
+PORT = 56377
 PORT1 = 12345
 
 #listening and accepting connection from client
@@ -22,94 +24,96 @@ s1.listen(5)
 c1,addr1 = s1.accept()
 print('connected to client1 ')
 
-#sending key object to client:
-c.send(tup.exportKey('PEM'))
-print('key object sent to client')
 
-#recieving encrypted file from client
-enc_data = c.recv(10000000) 
-enc_file = open('enc_file.txt','wb')
-enc_file.write(enc_data)
-enc_file.close()
-print('encrypted file recieved')
+for i in range(20):
+    #sending key object to client:
+    c.send(tup.exportKey('PEM'))
+    print('key object sent to client')
 
-#decrypting file from client
-f2 = open('enc_file.txt','rb')
-data = f2.read()
-data2 = RSA.pubkey.pubkey.decrypt(tup,data)
-f2.close()
-f = open('dec.txt','wb')
-f.write(data2)
-f.close()
-print('file from client decrypted')
+    #recieving encrypted file from client
+    enc_data = c.recv(10000000) 
+    enc_file = open('enc_file.txt','wb')
+    enc_file.write(enc_data)
+    enc_file.close()
+    print('encrypted file recieved')
 
-#recieving key object from client1:
-tup1 = c1.recv(2048)
-t = RSA.importKey(tup1)
-print('key object recieved key object from client1')
+    #decrypting file from client
+    f2 = open('enc_file.txt','rb')
+    data = f2.read()
+    data2 = RSA.pubkey.pubkey.decrypt(tup,data)
+    f2.close()
+    f = open('dec.txt','wb')
+    f.write(data2)
+    f.close()
+    print('file from client decrypted')
 
-#encrypting file again:
-f = open('dec.txt','rb')
-data = f.read()
-f.close()
-#encrypting 
-enc_data = RSA.pubkey.pubkey.encrypt(t,data,3)
-#storeD in enc_file.png
-f2 = open('enc_file1.txt','wb')
-f2.write(enc_data[0])
-f2.close()
-print('file encrypted')
+    #recieving key object from client1:
+    tup1 = c1.recv(2048)
+    t = RSA.importKey(tup1)
+    print('key object recieved key object from client1')
 
-#sending encrypted file:
-f3 = open('enc_file1.txt','rb')
-c1.sendfile(f3)
-print('encrypted file sent to client1')
+    #encrypting file again:
+    f = open('dec.txt','rb')
+    data = f.read()
+    f.close()
+    #encrypting 
+    enc_data = RSA.pubkey.pubkey.encrypt(t,data,3)
+    #storeD in enc_file.png
+    f2 = open('enc_file1.txt','wb')
+    f2.write(enc_data[0])
+    f2.close()
+    print('file encrypted')
 
-
-
-
+    #sending encrypted file:
+    f3 = open('enc_file1.txt','rb')
+    c1.sendfile(f3)
+    print('encrypted file sent to client1')
 
 
-#recieving acknowledgment file:
-ack_enc = c1.recv(100000000)
-f = open('ack_enc.txt','wb')
-f.write(ack_enc)
-f.close()
-print('ack recieved')
-
-#decrypting acknowledgement file:
-f2 = open('ack_enc.txt','rb')
-data = f2.read()
-data2 = RSA.pubkey.pubkey.decrypt(t,data)
-f2.close()
-f = open('ack_dec.txt','wb')
-f.write(data2)
-f.close()
-print('ack decrypted')
-
-#recieving key obj from client:
-tup2 = c.recv(2048)
-t2 = RSA.importKey(tup2)
-print('recieved key obj from client')
-
-#encrypting ack
-f = open('ack_dec.txt','rb')
-data = f.read()
-f.close()
-#encrypting 
-enc_data = RSA.pubkey.pubkey.encrypt(t2,data,3)
-#storeD in enc_file.png
-f2 = open('ack.txt','wb')
-f2.write(enc_data[0])
-f2.close()
-print('ack file encrypted')
 
 
-#sending acknowledgement to client:
-f2 = open('ack.txt','rb')
-c.sendfile(f2)
-f2.close()
-print('ack sent to client')
+
+
+    #recieving acknowledgment file:
+    ack_enc = c1.recv(100000000)
+    f = open('ack_enc.txt','wb')
+    f.write(ack_enc)
+    f.close()
+    print('ack recieved')
+
+    #decrypting acknowledgement file:
+    f2 = open('ack_enc.txt','rb')
+    data = f2.read()
+    data2 = RSA.pubkey.pubkey.decrypt(t,data)
+    f2.close()
+    f = open('ack_dec.txt','wb')
+    f.write(data2)
+    f.close()
+    print('ack decrypted')
+
+    #recieving key obj from client:
+    tup2 = c.recv(2048)
+    t2 = RSA.importKey(tup2)
+    print('recieved key obj from client')
+
+    #encrypting ack
+    f = open('ack_dec.txt','rb')
+    data = f.read()
+    f.close()
+    #encrypting 
+    enc_data = RSA.pubkey.pubkey.encrypt(t2,data,3)
+    #storeD in enc_file.png
+    f2 = open('ack.txt','wb')
+    f2.write(enc_data[0])
+    f2.close()
+    print('ack file encrypted')
+
+
+    #sending acknowledgement to client:
+    f2 = open('ack.txt','rb')
+    c.sendfile(f2)
+    f2.close()
+    print('ack sent to client')
 
 c.close()
 c1.close()
